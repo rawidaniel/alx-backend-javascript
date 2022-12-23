1e69f669d37588fd96d61074e8fc20f68d942af8
rawi daniel <rawidaniel96@gmail.com>
2022-12-23 15:12:25 +0000

    async await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,7 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  return { photo, user };
+}
