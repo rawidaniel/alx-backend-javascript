d3aefd819cf3c00723085976930ae8fc1a4368b3
rawi daniel <rawidaniel96@gmail.com>
2023-01-03 14:06:43 +0000

    Combine filter and map methods

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,8 +3,10 @@ export default function updateStudentGradeByCity(arr, city, newGrade) {
     .filter((ele) => ele.location === city)
     .map((ele) => {
       for (const { studentId, grade } of newGrade) {
-        if (studentId === ele.id) return { ...ele, grade };
-        return { ...ele, grade: 'N/A' };
+        if (ele.id === studentId) {
+          return { ...ele, grade };
+        }
       }
+      return { ...ele, grade: 'N/A' };
     });
 }
