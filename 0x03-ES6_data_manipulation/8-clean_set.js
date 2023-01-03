146d0ad1837073215961af4d66245b42d6c9cbca
rawi daniel <rawidaniel96@gmail.com>
2023-01-03 12:20:49 +0000

    fix bug

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,14 @@
 export default function cleanSet(set, startString) {
   const array = [];
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
   for (const ele of set.values()) {
-    if (ele.includes(startString) && startString) {
+    if (ele.startsWith(startString)) {
       array.push(ele.slice(startString.length));
     }
   }
