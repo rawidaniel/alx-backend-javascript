ab95edb2614f2089e3e15877d322ef6df122c224
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-22 20:15:24 +0300

    fix bug

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', function (data) {
-  if (data) process.stdout.write(`Your name is: ${data}`);
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) process.stdout.write(`Your name is: ${input}`);
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
