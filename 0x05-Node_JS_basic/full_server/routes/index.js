72f863c49b52eff750d1329830c96aedfa39a48a
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-23 00:49:05 +0300

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const router = express.Router();
+
+router.get('/', (request, response) => {
+  AppController.getHomepage(request, response);
+});
+
+router.get('/students', (request, response) => {
+  StudentsController.getAllStudents(request, response);
+});
+
+router.get('/students:majo', (request, response) => {
+  StudentsController.getAllStudentsByMajor(request, response);
+});
+module.exports = router;
