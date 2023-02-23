95db62a89d902297a165e49a091cf837ec75a712
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-23 17:15:11 +0300

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const expect = require('chai').expect;
+
+describe('getPaymentTokenFromAPI', function () {
+  it('getPaymentTokenFromAPI(true)', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        console.log(res);
+        expect(res).to.be.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
