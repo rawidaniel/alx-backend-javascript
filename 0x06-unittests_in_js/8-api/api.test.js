3312f60c870cad74e96b0d1215dcba2b543e7926
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-24 15:39:22 +0300

    fix bug

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,12 +2,13 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('Index page', () => {
-  it('request', () => {
+  it('request', (done) => {
     request('http://localhost:7865', (error, response, body) => {
       if (response) {
         expect(response.statusCode).to.equal(200);
         expect(response.statusMessage).to.equal('OK');
-        expect(response.body).to.equals('Welcome to the payment system');
+        expect(body).to.equals('Welcome to the payment system');
+        done();
       }
     });
   });
